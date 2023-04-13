3866701f97de51afab99596daca4d8fbefac64cd
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-04-12 22:35:40 -0400

    creado actions

--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,32 +1,46 @@
 import { createMachine } from "xstate";
 
-const bookingMachine = createMachine({
-  id: "Buy plane tickets",
-  initial: "initial",
-  states: {
-    initial: {
-      on: {
-        START: "search",
+const bookingMachine = createMachine(
+  {
+    id: "Buy plane tickets",
+    initial: "initial",
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "imprimirInicio",
+          },
+        },
       },
-    },
-    search: {
-      on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
+      search: {
+        entry: "imprimirEntrada",
+        exit: "imprimirSalida",
+        on: {
+          CONTINUE: "passengers",
+          CANCEL: "initial",
+        },
       },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: "initial",
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: "initial",
+        },
       },
-    },
-    tickets: {
-      on: {
-        FINISH: "initial",
+      tickets: {
+        on: {
+          FINISH: "initial",
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      imprimirInicio: () => console.log("imprimir inicio"),
+      imprimiEntrada: () => console.log("imprimir entreada a search"),
+      imprimiSalida: () => console.log("imprimir salida del search"),
+    },
+  }
+);
 
-export default bookingMachine
+export default bookingMachine;
