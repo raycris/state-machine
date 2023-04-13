b5a980341029f7ac902749ec0c1bcc66b058eb76
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-04-12 20:49:27 -0400

    mejora en el baselayout y steplayout

--- src/Containers/StepsLayout.jsx
+++ src/Containers/StepsLayout.jsx
@@ -1,22 +1,6 @@
-import React from 'react';
-import { Welcome } from '../Components/Welcome';
-import { Search } from '../Components/Search';
-import { Passengers } from '../Components/Passengers';
-import { Tickets } from '../Components/Tickets';
-import './StepsLayout.css';
+import React from "react";
+import "./StepsLayout.css";
 
-export const StepsLayout = ({ state, send }) => {
-  const renderContent = () => {
-    if(state.matches('initial')) return <Welcome send={send}/>;
-    if(state.matches('search')) return <Search send={send}/>;
-    if(state.matches('tickets')) return <Tickets send={send}/>;
-    if(state.matches('passengers')) return <Passengers send={send}/>;
-    return null;
-  };
-
-  return (
-    <div className='StepsLayout'>
-      {renderContent()}
-    </div>
-  );
+export const StepsLayout = (props) => {
+  return <div className="StepsLayout">{props.children}</div>;
 };
